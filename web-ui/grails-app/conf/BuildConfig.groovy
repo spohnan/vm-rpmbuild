54d7b1414c8f7b19f7237f0b6c0e72854a6a3c6e
Andy Spohn <Andy@AndySpohn.com>
2012-10-21 17:29:44 -0400

    As tomcat doesn't have an easy way to set the context name of a web app I'm removing the version number from the war file name

--- web-ui/grails-app/conf/BuildConfig.groovy
+++ web-ui/grails-app/conf/BuildConfig.groovy
@@ -4,6 +4,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
+grails.project.war.file = "target/${appName}.war"
 
 grails.project.dependency.resolution = {
 
